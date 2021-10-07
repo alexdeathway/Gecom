825ea193867b97e82a9b59c71c4f9e45fb8c0221
alexdeathway <alexdeathway@gmail.com>
2021-10-07 22:19:54 +0530

    added base and home

--- seco/urls.py
+++ seco/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from seco.views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index,name="home"),
+    path('', HomeView.as_view(),name="home"),
 
 ]
--- seco/views.py
+++ seco/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from games.models import GamesModel
+from django.views.generic import ListView
 
-def index(request):
-    return render("index.html")
+
+class HomeView(ListView):
+    template_name="home.html"
+    queryset=GamesModel.objects.all()
+
+#def index(request):
+#    return render(request,"index.html")
