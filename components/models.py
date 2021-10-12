e73f71f101e3da8147bd9b48519b15a58830faf2
alexdeathway <alexdeathway@gmail.com>
2021-10-12 19:53:13 +0530

    update index/home page

--- components/models.py
+++ components/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
+from django.db.models.base import Model
 # Create your models here.
+
+# User=get_user_model()
+
+class VendorModel(models.Model):
+    
+    name=models.CharField(unique=True,max_length=50)
+    email=models.EmailField(max_length=254)
