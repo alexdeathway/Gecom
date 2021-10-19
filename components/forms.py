459e832c6428a9bf9244915c33b0b49db91a3cee
alexdeathway <alexdeathway@gmail.com>
2021-10-19 18:49:10 +0530

    components views updated

---
+++ components/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from .models import ComponentsModel
+from games.models import PublisherModel as OrganisationModel
+
+
+class ComponentCreationForm(forms.ModelForm):
+      
+      def __init__(self,*args, **kwargs):
+            request=kwargs.pop("request")
+            vendor=OrganisationModel.objects.filter(owner=request.user)
+            super(ComponentCreationForm,self).__init__(*args,**kwargs)
+            self.fields["vendor"]=forms.ModelChoiceField(queryset=vendor)
+               
+      class Meta:
+         model=ComponentsModel
+         labels={
+            "vendor": "Vendor or your organisation",
+        }
+         fields=[
+            "name",
+            "category",
+            "cover",
+            "price",
+            "description",
+            "vendor"      
+         ]
