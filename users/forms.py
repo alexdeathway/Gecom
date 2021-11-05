93a8ccaa756fdfecd2a84f4da0aee538d57a4e87
alexdeathway <alexdeathway@gmail.com>
2021-11-05 23:26:37 +0530

    user update and UI modification

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
-#from django import forms 
+from django import forms 
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+
 #from users.models import User
 User=get_user_model()
 
@@ -17,3 +18,13 @@ class CustomUserCreationForm(UserCreationForm):
             "password1",
             "password2",
         ]
+
+class UserUpdateForm(forms.ModelForm):
+    
+    class Meta:
+        model =User
+        fields=[
+            "username",
+            "email",
+        ]
+    
--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import UserProfileView , testprofile
+from .views import (
+                        UserProfileView ,
+                        UserProfileUpdateView,
+                    )    
 app_name="users"
 
 urlpatterns = [
-    path("<str:username>/",UserProfileView.as_view(),name="profile")
+    path("<str:username>/",UserProfileView.as_view(),name="profile"),
+    path("<str:username>/update/",UserProfileUpdateView.as_view(),name="profileupdate")
 ]
 
