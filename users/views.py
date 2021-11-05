4e2c2be860143afe1b47a8a96db6d5ef31733b60
alexdeathway <alexdeathway@gmail.com>
2021-11-05 23:26:37 +0530

    user update and UI modification

--- users/views.py
+++ users/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render,reverse
 from django.views.generic import CreateView,DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .forms import CustomUserCreationForm
+from django.views.generic.edit import UpdateView
+from .forms import CustomUserCreationForm,UserUpdateForm
 from django.contrib.auth import get_user_model
+
+
 User=get_user_model()
-# Create your views here.
+
 
 class UserSignupView(CreateView):
     template_name="registration/signup.html"
@@ -28,6 +31,17 @@ class UserProfileView(LoginRequiredMixin ,DetailView):
     
 
 
+class UserProfileUpdateView(LoginRequiredMixin,UpdateView):
+    template_name="users/user_update.html"
+    #form_class=UserUpdateForm
+    model=User
+    form_class=UserUpdateForm
+    slug_url_kwarg="username"
+    slug_field="username"
+    
+    def get_success_url(self):
+        return reverse("home")
 
+    
 def testprofile(request):
     return render(request,"users/profile.html")
