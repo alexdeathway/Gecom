6149a252091561bae874b8c96bfb535fe9e11dc0
alexdeathway <alexdeathway@gmail.com>
2022-02-01 17:04:38 +0530

    profile org and local css update

--- games/urls.py
+++ games/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     path('organisation/create/', OrganisationCreateView.as_view(),name="organisationcreate"),
     path('<int:pk>/',GamesDetailView.as_view(),name="gamedetail"),
     path('<int:pk>/update/',GameUpdateView.as_view(),name="gameupdate"),
-    path("publisher/<int:pk>/",PublisherDetailView.as_view(),name="publisherdetail"),
+    path("publisher/<slug:username>/",PublisherDetailView.as_view(),name="publisherdetail"),
     path("organisation/<int:pk>/update/",OrganisationUpdateView.as_view(),name="organisationupdate"),
     path("category/<str:name>/",CategoryDetailView.as_view(),name="categorydetail"),
 ]
