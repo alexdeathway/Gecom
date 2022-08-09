d6363a42aad1a7fa2619b1f1d1e1ba0b2fd4f6ea
alexdeathway <alexdeathway@gmail.com>
2022-08-09 22:27:47 +0530

    docker add

--- seco/views.py
+++ seco/views.py
@@ -9,12 +9,12 @@ class HomeView(ListView):
     context_object_name="games"
     queryset=GamesModel.objects.all()[:8]
 
-    def get_context_data(self,**kwargs): 
-        context=super(HomeView,self).get_context_data(**kwargs)              
-        context["latest_release"]=GamesModel.objects.all().order_by('-release_date')[1]
-        context["games_sales"]=GamesModel.objects.filter(sale=True)[:3]
-        context["categories"]=CategoryModel.objects.all()[:4]
-        return context
+    def get_context_data(self,**kwargs):
+            context=super(HomeView,self).get_context_data(**kwargs)              
+            context["latest_release"]=GamesModel.objects.all().order_by('-release_date')[1]
+            context["games_sales"]=GamesModel.objects.filter(sale=True)[:3]
+            context["categories"]=CategoryModel.objects.all()[:4] 
+            return context
 
 #def index(request):
 #    return render(request,"index.html")
