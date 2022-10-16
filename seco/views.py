bdd5bce783c9c714bde71d27f95d3ebfe84cef04
alexdeathway <alexdeathway@gmail.com>
2022-10-16 13:01:21 +0530

    Error req and def

--- seco/views.py
+++ seco/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from games.models import GamesModel
 from games.models import CategoryModel
 from django.views.generic import ListView
-
+from django.http import Http404
 
 class HomeView(ListView):
     template_name="home.html"
@@ -10,10 +10,13 @@ class HomeView(ListView):
     queryset=GamesModel.objects.all()[:8]
 
     def get_context_data(self,**kwargs):
-            context=super(HomeView,self).get_context_data(**kwargs)              
-            context["latest_release"]=GamesModel.objects.all().order_by('-release_date')[1]
-            context["games_sales"]=GamesModel.objects.filter(sale=True)[:3]
-            context["categories"]=CategoryModel.objects.all()[:4] 
+            context=super(HomeView,self).get_context_data(**kwargs)
+            try:              
+                context["latest_release"]=GamesModel.objects.all().order_by('-release_date')[1]
+                context["games_sales"]=GamesModel.objects.filter(sale=True)[:3]
+                context["categories"]=CategoryModel.objects.all()[:4] 
+            except IndexError:
+                raise Http404("Homepage data missing,create superuser and dummy data in database!")
             return context
 
 #def index(request):
