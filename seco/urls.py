f33d6494f311396a08f318fc459d92828382d567
alexdeathway <alexdeathway@gmail.com>
2022-10-22 14:16:52 +0530

    added dummy data and change requirements

--- seco/urls.py
+++ seco/urls.py
@@ -47,6 +47,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     import debug_toolbar
+    
     urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     django_toolbar_panel = [
