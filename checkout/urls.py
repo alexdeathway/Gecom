213934cd42e20da4c8d6eb4f0f07a3f74e083b0e
alexdeathway <alexdeathway@gmail.com>
2024-05-11 18:27:04 +0530

    added cart and decoupled the cart view

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-from checkout.views import DemoNoPaymentView, UserCart,user_cart_item_count,PromoCodeView
+from checkout.views import (DemoNoPaymentView, UserCart,
+                            user_cart_item_count, PromoCodeView,
+                            add_to_cart,
+                            )
 
 app_name="checkout"
 
 urlpatterns = [
     path("nopurchase/",DemoNoPaymentView.as_view(),name="nopurchase"),
     path("usercart/",UserCart.as_view(),name="usercart"),
+    path("usercart/<int:id>/<str:type>/",add_to_cart,name="usercart_add"),
     path("usercartitemcount/",user_cart_item_count,name="usercartitemcount"),
     path("promocode/",PromoCodeView.as_view() ,name="promocode"),
 ]
