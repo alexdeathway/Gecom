bb9db4f18018edd37a3c7a3eaf27f5009b2b84a4
alexdeathway <alexdeathway@gmail.com>
2024-08-13 17:35:20 +0530

    removed redundant commands for runners and update certbot config

--- certbot/certify-init.sh
+++ certbot/certify-init.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-
-
 set -e
 
 echo "Getting certificate..."
@@ -14,3 +12,22 @@ certbot certonly \
     --rsa-key-size 4096 \
     --agree-tos \
     --noninteractive
+
+if [ $? -ne 0 ]; then
+    echo "Certbot encountered an error. Exiting."
+    exit 1
+fi
+
+#for copying the certificate and configuration to the volume
+if [ -f "/etc/letsencrypt/live/${DOMAIN}/fullchain.pem" ]; then
+    echo "SSL cert exists, enabling HTTPS..."
+    envsubst '${DOMAIN}' < /etc/nginx/nginx.prod.conf > /etc/nginx/conf.d/default.conf
+else
+    echo "Certbot unable to get SSL cert,server HTTP only..."
+fi
+
+
+echo "Setting up auto-renewal..."
+apk add --no-cache dcron
+echo "0 12 * * * certbot renew --quiet" | crontab -
+crond -f
