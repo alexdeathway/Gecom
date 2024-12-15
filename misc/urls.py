6b4bb6509f2d7a1dab80ffe43d4f9840dc6a08cb
alexdeathway <alexdeathway@gmail.com>
2024-12-16 00:34:52 +0530

    error pages update

--- misc/urls.py
+++ misc/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from misc.views import DevelopmentStatusView
+from misc.views import DevelopmentStatusView,Error404TestView,Error500TestView
+from django.conf import settings
 
 app_name="misc"
 
 urlpatterns=[
      path('development-status/',DevelopmentStatusView.as_view(),name="development-status"),
 ]
+
+#error pages in the development environment
+if settings.DEBUG:
+    urlpatterns+=[
+        path('404/',Error404TestView.as_view(),name="error404"),
+        path('500/',Error500TestView.as_view(),name="error500"),
+    ]
--- misc/views.py
+++ misc/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 #import view
-from django.views.generic import View
+from django.views.generic import View,TemplateView
 
 class DevelopmentStatusView(View):
       def get(self,request,*args,**kwargs):
           return render(request,"misc/development_status.html",{})
+
+class Error404TestView(TemplateView):
+    template_name = "errors/404.html"
+
+class Error500TestView(TemplateView):
+    template_name = "errors/500.html"
